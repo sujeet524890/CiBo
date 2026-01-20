61fdfcb9813900619182a115fee625127643b8d7
sujeet yadav <sujeeten1@gmail.com>
2026-01-21 02:15:07 +0530

    change main

--- CiBo/src/components/Main.jsx
+++ CiBo/src/components/Main.jsx
@@ -64,13 +64,13 @@ const Main = () => {
           </button>
           <button
             className={`${language.includes("Hindi") ? " bg-violet-500" : "bg-red-600"} text-white px-2 py-1 rounded-full hover:bg-blue-700 transition`}
-            onClick={() =>hendellanguag("")}
+            onClick={() =>hendellanguag("Hindi")}
           >
            Hindi
           </button>
           <button
-            className={`${language.includes("English")?' bg-fuchsia-800':'bg-red-600'} text-white px-2 py-1 rounded-full hover:bg-blue-700 transition`}
-            onClick={() =>hendellanguag("")}
+            className={`${language.includes("")?' bg-fuchsia-800':'bg-red-600'} text-white px-2 py-1 rounded-full hover:bg-blue-700 transition`}
+            onClick={() =>hendellanguag("English")}
           >
            English
           </button>
